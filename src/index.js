c1965f1e4df5d5829713e5ed5046024619843999
antonov-ser-v <antonov-ser-v@yandex.ru>
2019-09-30 02:21:22 +0900

    init: result

--- src/index.js
+++ src/index.js
@@ -4,11 +4,96 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
+
+
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
+  focus = 'family', 
+  knowsProgramming = true,
+  config = {
+    family: 4, 
+    friends: 10,
+    normal_life: 20,
+    profession: 30,
+    carrier: 40,
+    top_peformance: 60
+    }
+  ) 
+  {
+    var score_time = 800;
+    
+    var result;
   
+    
+      switch(focus)
+      {
+        
+        case "family":
+          if(knowsProgramming == true){
+          result = score_time / config.family;}
+          else{
+            result = 1300 / config.family;
+          }
+          
+          
+        break;
+
+        case "friends":
+            if(knowsProgramming == true)
+            {
+              result = score_time / config.friends;
+            }
+            else{
+              result = 1300 / config.friends;
+            }
+          
+          break;
+        case "normal_life":
+            if(knowsProgramming == true)
+            {
+              result = score_time / config.normal_life;
+            }
+            else
+            {
+              result = 1300 / config.normal_life;
+            }
+          
+        break;
+        case "profession":
+            if(knowsProgramming == true)
+            {
+              result = score_time / config.profession;
+            }
+            else
+            {
+              result = 1300 / config.profession;
+            }
+
+            
+        break;
+        case "carrier":
+            if(knowsProgramming == true)
+            {
+              result = score_time / config.carrier;
+            }
+            else{
+              result = 1300/ config.carrier;
+            }
+          
+        break;
+        case "top_peformance":
+            if(knowsProgramming == true)
+            {
+            result = score_time / config.top_peformance;
+            }
+            else{
+              result = 1300 / config.top_peformance;
+            }
+            
+        break;
+      }
+      return Math.ceil(result);
+     
+      
+      
+};
